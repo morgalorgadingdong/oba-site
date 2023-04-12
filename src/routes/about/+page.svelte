c8f825c373156fd3ae77ba97e236913aa58ba2bd
Morgan <morganfolz@gmail.com>
2023-04-12 07:58:31 -0700

    udpated about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,31 +1,31 @@
 <div class="vh-60 hero col-12 d-flex flex-column justify-content-end">
     <div class="img-container">
         <div class="img-overlay"></div>
-        <img class="hero-img img" src="./img/about.jpg"/>
+        <img class="hero-img img" src="./img/about2.jpg"/>
     </div>
     <div id="hero-tagline-container" class="d-flex justify-content-center flex-wrap p-3 pb-3 pb-md-5">
         <h1 class="hero-tagline col-12">About</h1>
     </div>
 </div>
 
-<section class="col-12 d-flex justify-content-center flex-wrap p-5">
+<section class="col-12 d-flex justify-content-center flex-wrap py-5">
     <!-- <h2>Our story</h2> -->
     <div class="col-12 col-md-10 col-lg-8">
-        <p>Oregon Bicycle Adventures is the brainchild of two friends, Carl and Morgan, who share a love for exploring the breathtaking outdoors on two wheels. Locally owned and operated out of Bend, we are ideally positioned to organize trips throughout the state, highlighting the finest experiences Oregon has to offer. Our mission is to provide both seasoned cyclists and beginners with a distinctive and genuine Oregon adventure. By keeping our group sizes small and tailoring each trip to our guests' preferences, we ensure that everyone can discover the sights and hidden treasures of Oregon at a comfortable pace. At Oregon Bicycle Adventures, we believe there is no better way to experience the world than from the saddle of a bicycle, and we are eager to share our passion for bike touring with you.</p>
+        <p>Oregon Bicycle Adventures, founded by friends Carl and Morgan, is a locally owned and operated bicycle tour company dedicated to helping you explore the stunning outdoors of Oregon on two wheels. Based out of the scenic city of Bend, we are ideally positioned to organize trips throughout the state, highlighting the finest experiences Oregon has to offer. Our mission is to provide both seasoned cyclists and beginners with a distinctive and genuine Oregon adventure. By keeping our group sizes small and tailoring each trip to our guests' preferences, we ensure that everyone can discover the sights and hidden treasures of Oregon at a comfortable pace. At Oregon Bicycle Adventures, we believe there is no better way to experience the world than from the saddle of a bicycle, and we are eager to share our passion for bike touring with you.</p>
 
     </div>
 </section>
 
-<section class="col-12 d-flex justify-content-center flex-wrap p-5">
+<section class="col-12 d-flex justify-content-center flex-wrap py-5 px-3">
     <h2>Get to know us</h2>
     
-    <div class="col-12 d-flex justify-content-between flex-wrap">
-        <div class="col-12 col-lg-4 p-3">
+    <div class="col-12 d-flex justify-content-between flex-wrap align-items-center mb-5">
+        <div class="col-12 col-lg-4 py-3 px-0">
             <img src="./img/carl.jpg" alt="Carl" class="img vh-50">
         </div>
-        <div class="col-12 col-lg-8 p-3">
-            <h3>Carl</h3>
-            <p>Co-owner and guide</p>
+        <div class="col-12 col-lg-8 py-3 px-0 px-md-5 d-flex align-items-center flex-wrap">
+            <h3 class="col-12 px-0">Carl</h3>
+            <p class="col-12 px-0">Co-owner and guide</p>
             <p>Carl has always been passionate about providing memorable experiences for others. His first venture began in 2016 with a winery tour company, where he discovered the joy of creating unique and enjoyable outings for his clients. After moving to Oregon in 2017, Carl quickly embraced the outdoor lifestyle, and the state's natural beauty became an integral part of his life. His enthusiasm, expertise, and dedication to curating exceptional adventures make him an invaluable member of our team, ensuring that each cycling tour is enjoyable and remarkable for our guests.</p>
             <!-- <ul>
                 <li>Never cried in a movie (self proclaimed)</li>
@@ -38,12 +38,12 @@
         </div>
     </div>
     <div class="col-12 d-flex justify-content-between flex-wrap align-items-center mb-5">
-        <div class="col-12 col-lg-4 p-3">
+        <div class="col-12 col-lg-4 py-3 px-0">
             <img src="./img/morgan.jpg" alt="Morgan" class="img vh-50">
         </div>
-        <div class="col-12 col-lg-8 p-3">
-            <h3>Morgan</h3>
-            <p>Co-owner and guide</p>
+        <div class="col-12 col-lg-8 py-3 px-0 px-md-5 d-flex align-items-center flex-wrap">
+            <h3 class="col-12 px-0">Morgan</h3>
+            <p class="col-12 px-0">Co-owner and guide</p>
             <p>Morgan, an Oregon native, has spent his life exploring the state's captivating landscape. From childhood camping trips to adult adventures, he knows Oregon like the back of his hand. In 2021, he made a bold decision to leave his career as a mechanical engineer and pursue his dream of biking across the country. This journey not only ignited a passion for cycling but also deepened his love for his home state. Morgan's extensive knowledge of Oregon and his commitment to cycling allow him to create authentic, engaging experiences for our guests.</p>
             <!-- <ul>
                 <li>Hometown: Salem,  Oregon</li>
