1db5b76b20d3ee87896afccc410f3bb703152a2b
Morgan <morganfolz@gmail.com>
2023-06-22 12:51:54 -0700

    test function

---
+++ src/routes/webhooks/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    // Add any necessary logic or imports here
+    import { onMount } from 'svelte';
+
+    // Make a request to the Netlify function on page load
+    onMount(async () => {
+      const response = await fetch('/.netlify/functions/webhooks');
+      // Handle the response as needed
+    });
+  </script>
+
+  <main>
+    <h1>Webhooks</h1>
+    <!-- Add the content for your webhooks page here -->
+  </main>
