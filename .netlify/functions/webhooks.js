50fd502bef5d4ba042c7727b22bd0d8c13995023
Morgan <morganfolz@gmail.com>
2023-06-22 12:40:25 -0700

    removed webhooks route

--- .netlify/functions/webhooks.js
+++ .netlify/functions/webhooks.js
@@ -24,62 +24,63 @@ exports.handler = async function(event, context) {
         console.log(storeItems)
         console.log('Retrieved store items');
         retrieveStoreItemImgs()
+        async function retrieveStoreItemImgs() {    
+            let index = 0;
+            let imgURL = []
+            //IN SERIES
+            for (const item of storeItems) {
+                let ids = item.itemData.imageIds
+                
+                if (ids.length == 1) {
+                    ids = String(ids)
+                    console.log(ids)
+                    try {
+                        const response = await client.catalogApi.retrieveCatalogObject(ids);
+                        imgURL.push(response.result.object.imageData.url) 
+                        console.log('Appended all img URLs')
+                    } catch(error) {
+                    console.log(error);
+                    }
+                } else if (ids.length > 1) {
+                    for (let id of ids) {
+                        id = String(id);
+                        console.log(id);
+                        try {
+                            console.log(`attempting to retrieve image with id: ${id}`);
+                            const response = await client.catalogApi.retrieveCatalogObject(id);
+                            imgURL.push(response.result.object.imageData.url);
+                            console.log('Appended all img URLs');
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    }
+                }
+                storeItems[index].itemData.imgURL = imgURL
+                index++
+            }
+            
+            
+            createJSONStoreItems()
+        }
+        
+        function createJSONStoreItems() {
+            //BigInt workaround
+            const json = JSON.stringify(storeItems, (key, value) =>
+                typeof value === "bigint" ? value.toString() + "n" : value
+                , 2);
+            // console.log(json)
+                fs.writeFile('src/routes/store-items.json', json, 'utf8', function(err) {
+                    console.log('File written')
+                    if (err) throw err;
+            });
+            console.log('Updated JSON Store File')
+        }
     } catch(error) {
         console.log(error);
     }
     }
 }
 
-async function retrieveStoreItemImgs() {    
-    let index = 0;
-    let imgURL = []
-    //IN SERIES
-    for (const item of storeItems) {
-        let ids = item.itemData.imageIds
-        
-        if (ids.length == 1) {
-            ids = String(ids)
-            console.log(ids)
-            try {
-                const response = await client.catalogApi.retrieveCatalogObject(ids);
-                imgURL.push(response.result.object.imageData.url) 
-                console.log('Appended all img URLs')
-            } catch(error) {
-            console.log(error);
-            }
-        } else if (ids.length > 1) {
-            for (let id of ids) {
-                id = String(id);
-                console.log(id);
-                try {
-                    console.log(`attempting to retrieve image with id: ${id}`);
-                    const response = await client.catalogApi.retrieveCatalogObject(id);
-                    imgURL.push(response.result.object.imageData.url);
-                    console.log('Appended all img URLs');
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        }
-        storeItems[index].itemData.imgURL = imgURL
-        index++
-    }
-    
-    
-    createJSONStoreItems()
-}
 
-function createJSONStoreItems() {
-    //BigInt workaround
-    const json = JSON.stringify(storeItems, (key, value) =>
-        typeof value === "bigint" ? value.toString() + "n" : value
-        , 2);
-    // console.log(json)
-        fs.writeFile('src/routes/store-items.json', json, 'utf8', function(err) {
-            console.log('File written')
-            if (err) throw err;
-    });
-    console.log('Updated JSON Store File')
-}
 
 // retrieveStoreItems()
